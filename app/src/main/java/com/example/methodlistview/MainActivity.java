aa49605362be00c9cbe02da473583695f236e9f1
Xiaodoubi2333 <crazysnail2133@gmail.com>
2024-06-10 14:21:41 +0800

    First commit submisson.

---
+++ app/src/main/java/com/example/methodlistview/MainActivity.java
@@ -0,0 +1,24 @@
+package com.example.methodlistview;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+
+public class MainActivity extends AppCompatActivity {
+
+    private String[] data ={"Apple","Banana"};
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(MainActivity.this,android.R.layout.simple_list_item_1,data);
+        ListView listView =(ListView) findViewById(R.id.list_view);
+        listView.setAdapter(adapter);
+    }
+}
